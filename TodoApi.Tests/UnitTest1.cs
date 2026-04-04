ac1768f5c97cd072f828b9c1dac1fdf35271cfbf
bakas-yannis <bakas.yannis@gmail.com>
2026-04-04 15:54:38 +0300

    Revert "Failed test"
    This reverts commit cdecd1eb9ba29de75685027098d69f61d7d669fc.

--- TodoApi.Tests/UnitTest1.cs
+++ TodoApi.Tests/UnitTest1.cs
@@ -6,6 +6,6 @@ public class UnitTest1
     [InlineData(5, 3)]
     public void Test1(int a, int b)
     {
-        Assert.True(a < b, $"{a} is not greater than {b}");
+        Assert.True(a > b, $"{a} is not greater than {b}");
     }
 }
